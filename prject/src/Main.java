58373b01c221c429cb88ba116bead6e68b798a03
Louis <louishuynh15@gmail.com>
2021-01-26 15:06:04 -0500

    PLEEEEEASE

--- prject/src/Main.java
+++ prject/src/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] arg){
         System.out.println("hello but no");
+
+        System.out.println("I BEG YOU GODS OF GIT HELP ME!");
+
+
     }
+
 }
