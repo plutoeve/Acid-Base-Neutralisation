0ee0f37f13aff0876689972244373711e158b5d0
gevor <gevorgalaverdyanmail@gmail.com>
2021-01-26 15:09:43 -0500

    fx added

--- prject/src/Main.java
+++ prject/src/Main.java
@@ -1,6 +1,16 @@
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-    public static void main(String[] arg){
+public class Main extends Application {
+
+    public static void main(String[] args){
         System.out.println("hello but no");
+        launch(args);
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
     }
 }
