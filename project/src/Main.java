921c2388253fd1595629119c444d6823619b32d1
Evelyn <wangyanyuan000@gmail.com>
2021-02-12 15:12:24 -0500

    3 packages and a controller class

--- project/src/Main.java
+++ project/src/Main.java
@@ -1,11 +1,17 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
 
     @Override
     public void start(Stage stage) throws Exception {
-
+       Pane pane = new GridPane();
+       Scene scene = new Scene(pane, 300, 300);
+       stage.setScene(scene);
+       stage.show();
     }
     public static void main(String[] arg){
         launch(arg);
