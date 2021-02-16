b6fc982caa8beea6bc22e7e835e18635b080091e
Evelyn <wangyanyuan000@gmail.com>
2021-02-16 14:55:00 -0500

    image

--- project/src/View/mainView.java
+++ project/src/View/mainView.java
@@ -4,30 +4,60 @@ import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 
 import javafx.scene.control.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class mainView {
 
-    GridPane pane = new GridPane();
-    Scene scene = new Scene(pane, 100,100);
-    Button startButton = new Button("start");
-    Button resetButton = new Button("reset");
 
-    public mainView() {
+        AnchorPane pane = new AnchorPane();
+        Scene scene = new Scene(pane, 400,400);
+        Button startButton = new Button("start");
+        Button resetButton = new Button("reset");
+        Label label = new Label("Simulation: Acid-base neutralization");
 
-        Main.firstStage.setScene(scene);
-        pane.getChildren().addAll(startButton);
-        startButton.setOnAction(start);
+        public mainView() {
+            pane.setPrefSize(300, 300);
+            Main.firstStage.setScene(scene);
+
+            label.setLayoutX(150);
+            label.setLayoutY(100);
+            startButton.setTranslateY(200);
+            startButton.setTranslateX(150);
+
+            pane.getChildren().add(label);
+            pane.getChildren().add(startButton);
+
+            startButton.setOnAction(start);
+            File file;
+            try {
+                FileInputStream input = new FileInputStream("resource\\background.png");
+                Image background = new Image(input);
+                ImageView bg = new ImageView(background);
+                pane.getChildren().add(bg);
+                bg.setLayoutX(200);
+                bg.setLayoutY(300);
+
+            }catch(FileNotFoundException e){
+                System.out.println("dumb ass");
+            };
 
 
+        }
+
+        EventHandler start = new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                Main.firstStage.setScene(simulationView.simulationScene);
+            }
+        };
     }
 
-    EventHandler start = new EventHandler() {
-        @Override
-        public void handle(Event event) {
-           Main.firstStage.setScene(simulationView.simulationScene);
-        }
-    };
-}
