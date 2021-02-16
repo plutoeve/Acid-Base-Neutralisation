bc7529092dc71a3905ef0809d4f1703011f53103
gevor <gevorgalaverdyanmail@gmail.com>
2021-02-16 15:57:55 -0500

    Reset button

--- project/src/View/SimulationView.java
+++ project/src/View/SimulationView.java
@@ -2,15 +2,22 @@ package View;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 
 public class SimulationView {
     Scene simulationScene;
     GridPane simulationPane = new GridPane();
+    Button resetButton = new Button("RESET");
+    ControlPanelView panelView = new ControlPanelView();
 
     public SimulationView(){
-
-        simulationPane.getChildren().add(new ControlPanelView());
+        resetButton.setOnAction(e->{
+            panelView.AcidBox.getSelectionModel().clearSelection();
+            panelView.BaseBox.getSelectionModel().clearSelection();
+        });
+        simulationPane.getChildren().add(panelView);
+        simulationPane.add(resetButton,1,1);
         simulationScene = new Scene(simulationPane,700 ,700 );
     }
 
