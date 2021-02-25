0b28770c6632bb8a9347cff03282fa48d610c9df
Louis Huynh <louishuynh15@gmail.com>
2021-02-25 12:12:14 -0500

    WHYYYYYYY part 2

--- project/src/View/HelpView.java
+++ project/src/View/HelpView.java
@@ -5,15 +5,28 @@ import javafx.scene.layout.VBox;
 import javafx.scene.Scene;
 import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class HelpView extends VBox {
 
-        Scene HelpScene;
+        File file;
+        Scanner scan;
+
+        {
+                try {
+                        new File("Help.txt");
+                        scan = new Scanner(file);
+                } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                }
+        }
 
 
         public HelpView(){
 
-                Text text = new Text("Welcome to our program on Acid-Base Titration! The following information is used" +
-                        "for our program.");
+                Text text = new Text(scan.nextLine());
                 this.getChildren().addAll(text);
 
 
