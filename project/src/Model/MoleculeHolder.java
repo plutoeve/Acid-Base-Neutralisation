0aa1acad2e826c5e75157eabb443bd1a8532016f
gevor <gevorgalaverdyanmail@gmail.com>
2021-02-25 11:44:50 -0500

    HashMAP

--- project/src/Model/MoleculeHolder.java
+++ project/src/Model/MoleculeHolder.java
@@ -7,7 +7,18 @@ public class MoleculeHolder {
 
 
     public MoleculeHolder() {
+    /***ACIDS***/
     MoleculeModel HydrochloricAcid = new AcidModel("HCl","Cl",36.458);
+    MoleculeModel HydrobromicAcid  = new AcidModel("HBr","Br",80.91);
+    MoleculeModel HydroiodicAcid   = new AcidModel("HI","I",127.911);
+    MoleculeModel SulfuricAcid = new AcidModel("H2SO4","SO4",98.079);
+    MoleculeModel NitricAcid  = new AcidModel("HNO3","NO3",63.01);
+    MoleculeModel PerchloricAcid   = new AcidModel("HClO4","ClO4",100.46 );
+    /***BASES***/
+    MoleculeModel SodiumHydroxide = new BaseModel("NaOH", "Na", 39.997 );
+    MoleculeModel PotassiumHydroxide = new BaseModel("KOH","K", 56.1056);
+    MoleculeModel LithiumHydroxide = new BaseModel("LiOH","Li",23.95);
+
 
     }
 
