8804c90f0bf2761641c803851d92e67712506f40
Louis Huynh <louishuynh15@gmail.com>
2021-03-18 16:48:58 -0400

    wHYYYYY PART 3

--- project/src/View/BlueFlask.java
+++ project/src/View/BlueFlask.java
@@ -23,7 +23,7 @@ public class BlueFlask extends BorderPane {
         imageView.setPreserveRatio(false);
         liquid.animate();
 
-
+        System.out.println("PRE SEQUEL");
 
         this.getChildren().addAll(imageView,liquid);
 
