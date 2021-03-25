c4fa69296a31e2ea97dd8d0d68be33e96d2cd73c
gevor <gevorgalaverdyanmail@gmail.com>
2021-03-25 13:32:37 -0400

    mainView button and text fixed

--- project/src/View/MainView.java
+++ project/src/View/MainView.java
@@ -8,46 +8,62 @@ import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
-
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 import java.io.FileNotFoundException;
 
 import java.io.FileNotFoundException;
 
-public class MainView extends GridPane {
+public class MainView {
 
 
-        AnchorPane pane = new AnchorPane();
+        GridPane pane = new GridPane();
         public static Scene scene;
-        Button startButton = new Button("start");
+        Button startButton = new Button("START");
 
         Label label = new Label("Simulation: Acid-Base Neutralisation");
-
+        VBox vbox = new VBox();
 
     public MainView() {
 
-        scene = new Scene(pane,1000 , 530);
+            scene = new Scene(pane,1000 , 530);
             Main.firstStage.setScene(scene);
 
 
 
-            label.setLayoutX(265);
-            label.setLayoutY(30);
-            label.setStyle("-fx-font: normal bold 30px 'serif' ;-fx-text-fill: beige;");
-            startButton.setTranslateY(200);
-            startButton.setTranslateX(150);
+            label.getStyleClass().add("outline");
+            scene.getStylesheets().addAll(getClass().getResource(
+                    "outline.css"
+            ).toExternalForm());
+            vbox.getChildren().add(label);
+            vbox.getChildren().add(startButton);
+            vbox.setAlignment(Pos.CENTER);
+            vbox.setSpacing(20);
 
-            pane.getChildren().add(label);
-            pane.getChildren().add(startButton);
-
+            pane.setAlignment(Pos.CENTER);
+            pane.getChildren().add(vbox);
+            setButtonStyle(startButton);
+            startButton.setScaleX(2);
+            startButton.setScaleY(2);
             startButton.setOnAction(start);
-            startButton.setTranslateX(500);
-            startButton.setTranslateY(265);
+
             Main.firstStage.setResizable(false);
         }
+
+
+        public void setButtonStyle(Button button){
+        startButton.setStyle("-fx-background-color:\n" +
+                "linear-gradient(#f0ff35, #a9ff00),\n" +
+                "radial-gradient(center 50% -40%, radius 200%, #b8ee36 45%, #80c800 50%);\n" +
+                "-fx-background-radius: 6, 5;\n" +
+                "-fx-background-insets: 0, 1;\n" +
+                "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.4) , 5, 0.0 , 0 , 1 );\n" +
+                "-fx-text-fill: #395306;");
+        }
+
+
 
         EventHandler start = new EventHandler() {
             @Override
