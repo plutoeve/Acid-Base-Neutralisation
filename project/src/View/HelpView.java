795ac0bc8c3d30ea897b5a5bed3d715f92adda41
Louis Huynh <louishuynh15@gmail.com>
2021-04-11 21:14:57 -0400

    Help Menu Clean

--- project/src/View/HelpView.java
+++ project/src/View/HelpView.java
@@ -1,25 +1,21 @@
 package View;
 
-import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
-import javafx.scene.Scene;
-import javafx.scene.text.Text;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
-import java.util.Scanner;
+
 
 public class HelpView extends VBox {
 
-        ScrollPane scroll = new ScrollPane();
-        InputStream stream = new FileInputStream("project/src/Resources/ProgramAcidBase.png");
-        Image image = new Image(stream);
-        ImageView imageView = new ImageView();
+        private ScrollPane scroll = new ScrollPane();
+        private InputStream stream = new FileInputStream("project/src/Resources/ProgramAcidBase.png");
+        private Image image = new Image(stream);
+        private ImageView imageView = new ImageView();
 
 
 
@@ -28,21 +24,6 @@ public class HelpView extends VBox {
                 scroll.setContent(imageView);
 
                 this.getChildren().add(scroll);
-
-
-         /*       try {
-                        File file = new File("project/src/View/unicorn.txt");
-                        Scanner scan = new Scanner(file);
-                        String message = "";
-                        while(scan.hasNextLine()){
-                                message = message + scan.nextLine()+("\n");
-                        }
-                        Text help = new Text(message);
-                        this.getChildren().add(help);
-                } catch (FileNotFoundException e) {
-                        System.out.println("file not found");;
-                }*/
-
 
         }
 
