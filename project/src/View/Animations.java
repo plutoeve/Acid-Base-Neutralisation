bd2749ae9903cef5b0f243d6359343f7384b934c
Evelyn <wangyanyuan000@gmail.com>
2021-04-11 20:32:20 -0400

    dark/light change

--- project/src/View/Animations.java
+++ project/src/View/Animations.java
@@ -4,13 +4,16 @@ import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.transform.Translate;
 import javafx.util.Duration;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.stream.Stream;
 
 public class Animations {
     public GridPane pane =  new GridPane();
@@ -38,5 +41,25 @@ public class Animations {
         redAnim.animate();
         beaker.animate();
 
+    }
+
+    public void toDark() throws FileNotFoundException{
+        blueAnim.flask.imageView.setImage(new Image(new FileInputStream("project/src/Resources/darkBlueFlask.png")));
+        redAnim.flask.imageView.setImage(new Image(new FileInputStream("project/src/Resources/darkRedFlask.png")));
+        beaker.emptyBeakerImageView.setImage(new Image(new FileInputStream("project/src/Resources/darkEmptyBeaker.png")));
+        beaker.fullBeakerImageView.setImage(new Image(new FileInputStream("project/src/Resources/darkBeaker.png")));
+
+        blueAnim.flask.text.setFill(Color.WHITE);
+        redAnim.flask.text.setFill(Color.WHITE);
+
+    }
+
+    public void toLight() throws FileNotFoundException{
+        blueAnim.flask.imageView.setImage(new Image(new FileInputStream("project/src/Resources/BlueFlask.png")));
+        redAnim.flask.imageView.setImage(new Image(new FileInputStream("project/src/Resources/RedFlask.png")));
+        beaker.emptyBeakerImageView.setImage(new Image(new FileInputStream("project/src/Resources/EmptyBeaker.png")));
+        beaker.fullBeakerImageView.setImage(new Image(new FileInputStream("project/src/Resources/Beaker.png")));
+        blueAnim.flask.text.setFill(Color.BLACK);
+        redAnim.flask.text.setFill(Color.BLACK);
     }
 }
